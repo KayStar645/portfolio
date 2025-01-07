1cf147f0cde082c0ca577a11c599976beca589e3
KayStar <thuanpt182@gmail.com>
2025-01-07 23:05:17 +0700

    init component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,17 @@
-import { Component, inject } from '@angular/core';
-import {TranslateModule, TranslateService} from '@ngx-translate/core';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [TranslateModule],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  translate: TranslateService = inject(TranslateService);
-
-  translateText(lang: string) {
-    this.translate.use(lang);
-  }
 }
