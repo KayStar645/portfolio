2f7e4ba71ff5ecd34e64a94550aba5fe6543582b
KayStar <thuanpt182@gmail.com>
2025-01-10 23:49:52 +0700

    commit temp

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  config: {
+    api: false,
+    imageUrl: window.location.origin + '/images/',
+    url: './assets/params/json/crud/',
+  },
+  backendAdminUrl: '',
+};
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,14 +1,56 @@
 import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { environment } from '../../../environments/environment';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [TranslateModule],
+  imports: [TranslateModule, RouterLink, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   translate: TranslateService = inject(TranslateService);
+  imageUrl = environment.config.imageUrl;
+
+  user = {
+    name: "Phạm Tấn Thuận",
+    avatar: "http://localhost:4200/avatar.jpg"
+  };
+
+  items = [
+    {
+      name: "Skills",
+      icon: "fas fa-book-dead",
+      link: '/skill'
+    },
+    {
+      name: "Projects",
+      icon: "fas fa-project-diagram",
+      link: '/project'
+    },
+    {
+      name: "Experience",
+      icon: "fas fa-tasks",
+      link: '/experience'
+    },
+    {
+      name: "Education",
+      icon: "fas fa-university",
+      link: '/education'
+    },
+    {
+      name: "Achievements",
+      icon: "fas fa-trophy",
+      link: '/achievement'
+    },
+    {
+      name: "Resume",
+      icon: "fas fa-address-card",
+      link: '/resume'
+    },
+  ];
 
   translateText(lang: string) {
     this.translate.use(lang);
