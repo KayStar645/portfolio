b056ea34e3d256a7c444b3dd7a6fb71fc6b6430b
KayStar <thuanpt182@gmail.com>
2025-02-14 01:29:10 +0700

    chore: remove test:ci

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,40 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-// xdescribe('AppComponent', () => {
-//   xit('should create the app', () => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     const app = fixture.componentInstance;
-//     expect(app).toBeTruthy();
-//   });
-// });
-
-export function createTranslateLoader(http: HttpClient) {
-  return new TranslateHttpLoader(http, './i18n/', '.json');
-}
-
-describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        HttpClientModule,
-        TranslateModule.forRoot({
-          loader: {
-            provide: TranslateLoader,
-            useFactory: createTranslateLoader,
-            deps: [HttpClient],
-          },
-          defaultLanguage: 'vi-VN',
-        }),
-        AppComponent,
-      ],
-    }).compileComponents();
-  });
-
-  it('should create the app', () => {
+xdescribe('AppComponent', () => {
+  xit('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
