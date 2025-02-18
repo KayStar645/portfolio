71edd8b4d9fcd7812ef764369e7a2685bab7108d
KayStar <thuanpt182@gmail.com>
2025-02-19 01:30:54 +0700

    feat: add ui page skill

--- src/app/features/general/skill/skill.component.ts
+++ src/app/features/general/skill/skill.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { GroupedSkill, SkillService } from './../../../services/skill.service';
 
 @Component({
   selector: 'app-skill',
-  imports: [],
+  imports: [
+    CommonModule,
+    TranslateModule,
+  ],
   templateUrl: './skill.component.html',
   styleUrl: './skill.component.scss'
 })
-export class SkillComponent {
+export class SkillComponent implements OnInit {
+  translate: TranslateService = inject(TranslateService);
+  groupedSkills: GroupedSkill[] = [];
 
+  constructor(
+    private skillService: SkillService,
+  ) { }
+
+  async ngOnInit(): Promise<void> {
+    try {
+      this.groupedSkills = await this.skillService.getGroupedItems();
+    } catch (error) {
+      this.groupedSkills = [];
+    }
+  }
 }
