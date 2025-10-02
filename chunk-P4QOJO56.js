427188518899cd4b2559fff01b932c8832804265
KayStar645 <KayStar645@users.noreply.github.com>
2025-10-02 18:26:40 +0000

    deploy: ab353d6448aa131db58d22e483c653cc0868831e

--- chunk-P4QOJO56.js
+++
@@ -1,1 +0,0 @@
-import{Ta as n,jb as o,kb as s,xb as m}from"./chunk-BIC54VRN.js";var a=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=n({type:t,selectors:[["app-resume"]],decls:2,vars:0,template:function(e,r){e&1&&(o(0,"p"),m(1,"summary works!"),s())},encapsulation:2})};export{a as ResumeComponent};
